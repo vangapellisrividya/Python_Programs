56a5fbd6946e03da64f19f47cc2d7d69da14616f
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-11-24 18:56:05 +0530

    [Srividya] Add: Calculating the power of 2

---
+++ Basic_programs/PowerOf2.py
@@ -0,0 +1,16 @@
+'''
+    Created on Nov 16, 2021
+
+    @author:srividya
+    @Date:  16/11/2021
+    @Title: Power Of 2
+
+    '''
+
+if __name__=='__main__':
+    value = int(input("enter a value "))
+    if 0<value<31:
+        result =int( 2 ** value)
+    print("value is: ",result)
+    
+    
